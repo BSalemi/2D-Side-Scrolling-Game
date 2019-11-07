2b92a3e0c55b8ad8a73cf49440ea984c8e212cc0
Brian <briandsalemi@gmail.com>
2019-11-07 18:36:50 -0500

    setting up the player icon and moving capabilities for the player

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
-const context = document.querySelector("canvas").getContext("2d");
+let context, controller, player, loop;
+
+
+context = document.querySelector("canvas").getContext("2d");
+
+context.canvas.height = 180;
+context.canvas.width = 320;
+
+player = {
+
+    height:32,
+    jumping: true,
+    width: 32,
+    x: 144,
+    x_velocity: 0,
+    y: 0,
+    y_velocity: 0
+
+};
+
+controller = {
+
+    left: false,
+    right: false,
+    up: false,
+    keyListener: function(event) {
+        var key_state = (event.type == "keydown") ? true: false;
+
+        switch(event.keyCode) {
+
+            case 37: 
+                controller.left = key_state;
+            break;
+            case 38:
+                controller.up = key_state;
+            break;
+            case 39:
+                controller.right = key_state;
+            break;
+
+        }
+    }
+};
+
+loop = function(){
+
+    if (controller.up && rectangle.jumping == false) {
+
+        player.y_velocity -= 20;
+        player.jumping = true;
+
+    }
+
+    if (controller.left) {
+
+        player.x_velocity -= 0.5;
+
+    }
+
+    if (controller.right) {
+
+        player.x_velocity += 0.5; 
+        
+    }
+
+}
 
 const render = function(){
 
